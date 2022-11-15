09c440194fc083cef9697e3cc1b8262eb6a4f8ba
M Nu'man Arif H <numanarif87@gmail.com>
2022-11-16 01:04:01 +0700

    setup connection

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-const app = () => {
-  console.log("object");
-};
+import express from "express";
+import request from "request-promise";
+
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`);
+});
