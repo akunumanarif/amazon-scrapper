609751b5826446453ea452fb9ee6d2736ecdcdb8
M Nu'man Arif H <numanarif87@gmail.com>
2022-11-16 01:14:20 +0700

    setup apikey and baseUrl from scrapi api

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ import request from "request-promise";
 const app = express();
 app.use(express.json());
 
+const api_key = "6651bdf3da4e6f978b7fd9361d270aea";
+const baseUrl = `https://api.scraper.com?api_key${api_key}&autoparse=true`;
+
+app.get("/", (req, res) => {
+  res.send("Welcome to amazon scraper API");
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
